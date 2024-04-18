fe7acb04b211bacd5444df52d8d3741d2498e6e8
Mateusz-Lach <lach.mateusz88@gmail.com>
2024-04-18 21:27:08 +0200

    working on posts

---
+++ Mountains-Forum/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mountains_Forum.Models;
+using Mountains_Forum.Services;
+
+namespace Mountains_Forum.Controllers
+{
+    [ApiController]
+    [Route("mountains/post")]
+    public class PostController : ControllerBase
+    {
+        public PostController()
+        {
+
+        }
+        [HttpGet("{topicId}")]
+        public ActionResult GetAllPosts([FromRoute] int topicId)
+        {
+            return Ok(topicId);
+        }
+    }
+}
---
+++ Mountains-Forum/Services/PostService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mountains_Forum.Entities;
+using Mountains_Forum.Exceptions;
+using Mountains_Forum.Models;
+
+namespace Mountains_Forum.Services
+{
+    public class PostService
+    {
+        private readonly ForumDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public PostService(ForumDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+        public IEnumerable<PostDto> GetAlGetAllPostsByTopicId(int topicId)
+        {
+
+        }
+    }
+}
